ca70e3eff148bbd468165aeda7fd2a18fd7a09bb
xiaoxiang9527 <xiang.xiao@outlook.com>
2019-02-18 16:03:09 +0800

    1

--- src/apis.js
+++ src/apis.js
@@ -1,5 +1,5 @@
-export const recommendPoetry="http://api.apiopen.top/recommendPoetry"
-export const authorSearch="http://api.apiopen.top/searchAuthors?name="
-export const poetrySearch="http://api.apiopen.top/searchPoetry?name="
+export const recommendPoetry="https://api.apiopen.top/recommendPoetry"
+export const authorSearch="https://api.apiopen.top/searchAuthors?name="
+export const poetrySearch="https://api.apiopen.top/searchPoetry?name="
 export const likeSearch="https://api.apiopen.top/likePoetry?name="
 export const site="xiaoxiang.online"
